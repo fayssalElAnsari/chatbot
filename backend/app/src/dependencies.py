0641b1de5ff77063cf51008f8813707d575de088
Fayssal EL ANSARI <fayssal.el.ansari@gmail.com>
2024-08-22 15:08:53 +0200

    RD-64: add route ingest singular .md file

--- backend/app/src/dependencies.py
+++ backend/app/src/dependencies.py
@@ -5,3 +5,6 @@ def get_llm(request: Request):
 
 def get_query_engine(request: Request):
     return request.app.state.query_engine
+
+def get_vector_store(request: Request):
+    return request.app.state.vector_store
